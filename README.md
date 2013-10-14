c7cbe852a4a16dd444a2edc43a6c98b990469480
Ryan Rich <rrich@globalthinking.com>
2013-10-14 17:19:38 -0400

    initial source commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pillar of Faith
+
+A somewhat minimal theme for the [Ghost](http://ghost.org) blogging platform.
---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        // On the home page, move the blog icon inside the header
+        // for better relative/absolute positioning.
+
+        //$("#blog-logo").prependTo("#site-head-content");
+
+    });
+
+}(jQuery));
